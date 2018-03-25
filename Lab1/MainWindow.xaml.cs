27c4cdd8caaa40413118ce52c77dd6a550b20169
mihan1235 <mihan1235@yandex.ru>
2018-03-25 21:56:05 +0300

    CollectionViewSource binding now works

--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using library;
+using Microsoft.Win32;
 
 namespace Lab1
 {
@@ -23,6 +25,96 @@ namespace Lab1
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void FilterByResearcher(object sender, FilterEventArgs e)
+        {
+            if (e.Item is Researcher)
+            {
+                e.Accepted = true;
+            }
+            else
+            {
+                e.Accepted = false;
+            }
+         
+        }
+
+        private void FilterByProgrammer(object sender, FilterEventArgs e)
+        {
+            if (e.Item is Programmer)
+            {
+                e.Accepted = true;
+            }
+            else
+            {
+                e.Accepted = false;
+            }
+        }
+
+        private void DO_add_def_person(object sender, RoutedEventArgs e)
+        {
+            var obj = (DepartmentObservable) this.FindResource("key_department");
+            obj.AddDefaultPerson();
+        }
+
+        private void DO_add_def_researcher(object sender, RoutedEventArgs e)
+        {
+            var obj = (DepartmentObservable)this.FindResource("key_department");
+            obj.AddDefaultResearcher();
+        }
+
+        private void make_custom_programmer()
+        {
+            DialogCustomProgrammer dialog = new DialogCustomProgrammer();
+            if (dialog.ShowDialog() == true)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+        private void add_custom_programmer(object sender, RoutedEventArgs e)
+        {
+            make_custom_programmer();
+        }
+
+        private void context_menu_custom_pr(object sender, RoutedEventArgs e)
+        {
+            make_custom_programmer();
+        }
+
+        private void new_make_DOCollection(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void open_DOCollection(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open_diag = new OpenFileDialog();
+            if (open_diag.ShowDialog() == true)
+            {
+
+            }
+        }
+
+        private void save_DOCollection(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog save_diag = new SaveFileDialog();
+            if (save_diag.ShowDialog() == true)
+            {
+
+            }
+        }
+
+        private void DO_add_def_programmer(object sender, RoutedEventArgs e)
+        {
+            var obj = (DepartmentObservable)this.FindResource("key_department");
+            obj.Add(new Programmer());
         }
     }
 }
--- library/DepartmentObservable.cs
+++ library/DepartmentObservable.cs
@@ -9,6 +9,67 @@ namespace library
 {
     public class DepartmentObservable : ObservableCollection<Person>
     {
+        bool collection_changed;
+        public bool DOCollectionChanged
+        {
+            get
+            {
+                return collection_changed;
+            }
+            set
+            {
+                collection_changed = value;
+            }
+        }
+        public bool PercentageOfResearchers
+        {
+            get;
+            set;
+        }
 
+        List<Paper> PapersSet;
+
+        public DepartmentObservable()
+        {
+            //CollectionChanged += detect_collection_changed();
+            AddDefaultPerson();
+            AddDefaultPerson();
+            AddDefaultPerson();
+        }
+
+        private void detect_collection_changed()
+        {
+
+        }
+
+        public void AddDefaultPerson()
+        {
+            Add(new Person());
+        }
+
+        public void AddDefaultResearcher()
+        {
+            Add(new Researcher());
+        }
+
+        public void AddDefaultProgrammer()
+        {
+            Add(new Programmer());
+        }
+
+        public void AddProgrammer(Programmer pr)
+        {
+            Add(pr);
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach(var item in Items)
+            {
+                str += item.ToString();
+            }
+            return str;
+        }
     }
 }
